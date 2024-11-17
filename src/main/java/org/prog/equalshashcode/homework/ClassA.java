11020bd9689206c73b6fcb2f2f80b9f52a07f3f2
AnnaSavchenko2304 <ulckiev@gmail.com>
2024-11-17 23:00:18 +0100

    HomeworkPhone-1 homework 1.1 -interface homework 1.2 - OverrideOverload. The both code are works.

---
+++ src/main/java/org/prog/equalshashcode/homework/ClassA.java
@@ -0,0 +1,7 @@
+package org.prog.equalshashcode.homework;
+
+class ClassA {
+    public void baseMethod() {
+        System.out.println("Base method in ClassA");
+    }
+}
--- src/main/java/org/prog/equalshashcode/homework/OverrideOverloadHomework.java
+++ src/main/java/org/prog/equalshashcode/homework/OverrideOverloadHomework.java
@@ -10,5 +10,23 @@ package org.prog.equalshashcode.homework;
  * Add method with overload to any class with at least 3 samples
  * method(a), method(a,b) method(a,b,c)
  */
+
 public class OverrideOverloadHomework {
+    public static void main(String[] args) {
+        ClassA objA = new ClassA();
+        ClassB objB = new ClassB();
+        ClassC objC = new ClassC();
+        ClassD objD = new ClassD();
+
+        // Testing overridden methods
+        objA.baseMethod(); // Calls ClassA's method
+        objB.baseMethod(); // Calls ClassB's overridden method
+        objC.baseMethod(); // Calls ClassB's overridden method (inherited)
+        objD.baseMethod(); // Calls ClassD's overridden method
+
+        // Testing overloaded methods in ClassD
+        objD.overloadedMethod(1);
+        objD.overloadedMethod(1, 2);
+        objD.overloadedMethod(1, 2, 3);
+    }
 }
