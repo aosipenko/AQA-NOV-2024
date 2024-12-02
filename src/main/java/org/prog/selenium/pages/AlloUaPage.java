17467ff83f645a1ba774234ef1ea20556ea72cc3
aosipenko <aoskiev@gmail.com>
2024-12-02 20:14:01 +0100

    Session 7: Selenium pt 2, Page Objects, Abstract Classes

---
+++ src/main/java/org/prog/selenium/pages/AlloUaPage.java
@@ -0,0 +1,7 @@
+package org.prog.selenium.pages;
+// add method which checks goods id on position X
+// if X < 0 -> throw exception
+// if X > goods count -> throw exception
+// if 0 <= X <= goods_amount -> scroll to item, then print goods id for that item
+public class AlloUaPage {
+}
