5418579672b261398f3a22158be353fc24f0bedf
aosipenko <aoskiev@gmail.com>
2024-12-20 19:20:57 +0100

    Session 12: Selenoid + Docker SQL

--- src/test/java/org/prog/cucumber/CucumberRunner.java
+++ src/test/java/org/prog/cucumber/CucumberRunner.java
@@ -4,16 +4,23 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import lombok.SneakyThrows;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariOptions;
 import org.prog.cucumber.steps.GoogleSteps;
 import org.prog.cucumber.steps.SQLSteps;
 import org.prog.selenium.pages.GooglePage;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 @CucumberOptions(
         glue = "org.prog.cucumber.steps",
@@ -29,11 +36,13 @@ public class CucumberRunner extends AbstractTestNGCucumberTests {
     private Connection connection;
 
     @BeforeSuite
-    public void setUp() throws SQLException {
+    public void setUp() throws SQLException, MalformedURLException {
         connection = DriverManager.getConnection
-                ("jdbc:mysql://localhost:3306/db", "user", "password");
+                ("jdbc:mysql://mysql-db-1:3306/db", "user", "password");
 
-        driver = new ChromeDriver();
+        driver = new RemoteWebDriver(
+                new URL("http://selenoid-selenoid-1:4444/wd/hub"), remoteChrome());
+
         GoogleSteps.googlePage = new GooglePage(driver);
         SQLSteps.connection = connection;
     }
@@ -47,5 +56,18 @@ public class CucumberRunner extends AbstractTestNGCucumberTests {
         if (driver != null) {
             driver.quit();
         }
+    }
+
+    private ChromeOptions remoteChrome() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setAcceptInsecureCerts(true);
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            put("enableVideo", true);
+            put("enableVNC", true);
+        }});
+        return chromeOptions;
     }
 }
